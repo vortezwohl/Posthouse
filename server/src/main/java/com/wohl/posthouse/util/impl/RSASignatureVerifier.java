1efd5d9b2f10775f6b4a23a3471344d169232040
= <=>
2024-05-21 17:58:14 +0800

    改造当前的传输层通信，添加ssl握手（使用netty）,给部分代码添加注释

--- server/src/main/java/com/wohl/posthouse/util/impl/RSASignatureVerifier.java
+++ server/src/main/java/com/wohl/posthouse/util/impl/RSASignatureVerifier.java
@@ -22,14 +22,14 @@ public class RSASignatureVerifier implements SignatureVerifier {
         Signature verifier = Signature.getInstance("RSA", "BC");
         byte[] decodedKey = Base64.getDecoder().decode(publicKey);
 
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decodedKey);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decodedKey);// 创建一个 X509EncodedKeySpec，它包含X.509标准的公钥编码。
 
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA", "BC");
-        PublicKey receivedPublicKey = keyFactory.generatePublic(keySpec);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA", "BC");//创建实例
+        PublicKey receivedPublicKey = keyFactory.generatePublic(keySpec);//生成公钥实例
 
-        verifier.initVerify(receivedPublicKey);
+        verifier.initVerify(receivedPublicKey);//初始化签名器
         verifier.update(msgBody.getBytes(CharsetUtil.UTF_8));
 
-        return verifier.verify(Base64.getDecoder().decode(signature));
+        return verifier.verify(Base64.getDecoder().decode(signature));//验签
     }
 }
