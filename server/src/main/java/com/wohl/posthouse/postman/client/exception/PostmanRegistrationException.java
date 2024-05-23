f377455915d9c0fd092154d34e58c00b526cf68b
吴子豪 <vortezwohl@proton.me>
2024-05-23 16:48:40 +0800

    feat: 服务端添加消息构建模块

---
+++ server/src/main/java/com/wohl/posthouse/postman/client/exception/PostmanRegistrationException.java
@@ -0,0 +1,10 @@
+package com.wohl.posthouse.postman.client.exception;
+
+/*
+ * @Author 吴子豪
+ */
+public class PostmanRegistrationException extends RuntimeException {
+    public PostmanRegistrationException(String msg) {
+        super(msg);
+    }
+}
